6930dd588a99aaec589dc966a4b3d25c8ba1adb2
Wendel <renanwendel100@gmail.com>
2025-08-28 09:10:36 -0300

    version 1.3

--- obj/Debug/net9.0/Minimal-Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Minimal-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minimal-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c1bb0b6e9a4e374a788a91c501d6a2d1f784fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1502ac4462392dbe198e96849358d08de3dafa6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minimal-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minimal-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
